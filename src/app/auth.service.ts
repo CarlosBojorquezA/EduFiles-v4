88252a5cdd19d970d3a3230783d3d09b47ba4ec8
Carlos Bojórquez Apodaca <143865041+CarlosBojorquezA@users.noreply.github.com>
2025-12-03 23:10:54 -0700

    haciendo deploy

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from '../../src/environments/environment.development';
 
 interface RegistroData {
   nombres: string;
@@ -22,7 +23,7 @@ interface RegistroData {
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000/api';
+  private apiUrl = environment.apiUrl;
   private currentUserSubject = new BehaviorSubject<any>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
